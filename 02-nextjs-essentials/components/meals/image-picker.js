0f5f503a4c26dfab77bb183cd0786daa59c7eaab
dirk <dirk@maplelane.ag>
2024-08-06 15:13:03 -0400

    117. Getting Started with a Custom Image Picker Input Component

---
+++ 02-nextjs-essentials/components/meals/image-picker.js
@@ -0,0 +1,37 @@
+"use client";
+
+import { useRef } from "react";
+import classes from "./image-picker.module.css";
+
+export default function ImagePicker({ label, name }) {
+  const imageInput = useRef();
+
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+        />
+        {/* Remember: when using event handlers you MUST set the "user client" 
+        directive */}
+        <button
+          onClick={handlePickClick}
+          className={classes.button}
+          type="button"
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
