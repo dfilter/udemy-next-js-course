1b2bea33b359df7439ff458a5885a71b846baa34
dirk <dirk@maplelane.ag>
2024-08-06 11:13:24 -0400

    101. Optimizing Images with the NextJS Image Component

--- 02-nextjs-essentials/components/main-header.js
+++ 02-nextjs-essentials/components/main-header.js
@@ -1,16 +1,20 @@
 import Link from "next/link";
+import Image from "next/image";
 
 // unlike regular react imported images will be an object.
 import logoImg from "@/assets/logo.png";
 
+import classes from "./main-header.module.css";
+
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
-        <img src={logoImg.src} alt="A plate with food on it." />
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
+        {/* <Image> provides useful functionality like lazy loading etc. */}
+        <Image src={logoImg} alt="A plate with food on it." priority />
         NextLevel Food
       </Link>
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
