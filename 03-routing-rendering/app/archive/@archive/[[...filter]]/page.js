1a2026e6ffa968cee5f852e776e179e1bf234d92
dirk <dirk@maplelane.ag>
2024-08-07 12:29:37 -0400

    146. Handling Errors With Error Pages

--- 03-routing-rendering/app/archive/@archive/[[...filter]]/page.js
+++ 03-routing-rendering/app/archive/@archive/[[...filter]]/page.js
@@ -34,9 +34,9 @@ export default function FilteredNewsPage({ params }) {
   }
 
   if (
-    (selectedYear && !getAvailableNewsYears().includes(selectedYear)) ||
+    (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
     (selectedMonth &&
-      !getAvailableNewsMonths(selectedYear).includes(selectedMonth))
+      !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
   ) {
     throw new Error("Invalid filter.");
   }
