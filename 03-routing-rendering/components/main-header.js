add297e9f55eca6920ca9a43b96b0c0be7e9df5a
dirk <dirk@maplelane.ag>
2024-08-07 10:52:28 -0400

    138. Exercise Solution - Part 2

--- 03-routing-rendering/components/main-header.js
+++ 03-routing-rendering/components/main-header.js
@@ -3,16 +3,14 @@ import Link from "next/link";
 export default function MainHeader() {
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/news">News</Link>
-          </li>
-        </ul>
-      </nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/news">News</Link>
+        </li>
+      </ul>
     </header>
   );
 }
