5aace3135159e563f362251735bb7c2aa0749542
dirk <dirk@maplelane.ag>
2024-08-09 09:59:21 -0400

    219. Adding User Login (via a Server Action)

--- 08-authentication/actions/auth-actions.js
+++ 08-authentication/actions/auth-actions.js
@@ -2,8 +2,8 @@
 
 import { redirect } from "next/navigation";
 
-import { hashUserPassword } from "@/lib/hash";
-import { createUser } from "@/lib/user";
+import { hashUserPassword, verifyPassword } from "@/lib/hash";
+import { createUser, getUserByEmail } from "@/lib/user";
 import { createAuthSession } from "@/lib/auth";
 
 export async function signup(prevState, formData) {
@@ -42,4 +42,32 @@ export async function signup(prevState, formData) {
     }
     throw error;
   }
+}
+
+export async function login(previousState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const existingUser = getUserByEmail(email);
+
+  if (!existingUser) {
+    return {
+      errors: {
+        email: "Could not authenticate user, please check your credentials.",
+      },
+    };
+  }
+
+  const isValidPassword = verifyPassword(existingUser.password, password);
+
+  if (!isValidPassword) {
+    return {
+      errors: {
+        password: "Could not authenticate user, please check your credentials.",
+      },
+    };
+  }
+
+  await createAuthSession(existingUser.id);
+  redirect("/training");
 }
